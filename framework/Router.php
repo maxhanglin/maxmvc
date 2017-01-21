e396bef35acaba9e7b8cf7e79195ab784c18378c
Maximiliano Hanglin <mhanglin@gmail.com>
2017-01-21 16:04:42 -0300

    Redirect method added to the router

--- framework/Router.php
+++ framework/Router.php
@@ -58,6 +58,12 @@ class Router {
 		return $uri_2;
 	}
 
+	static function redirect($url, $permanent = false)
+	{
+	    header('Location: ' . $url, true, $permanent ? 301 : 302);
+	    exit();
+	}
+
 	private function getRouteParts($route) {
 		if (is_array($route)) {
 			$route = $route["url"];
